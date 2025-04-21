93fa3976a23cba88fc159ea54c2e6dfdab930ed3
Mariano Arias <arias.mariano.ar@gmail.com>
2025-04-20 21:09:57 -0300

    importing StoredProcedures and Functions, working with Enums

--- DbFirst/DbFirst/Program.cs
+++ DbFirst/DbFirst/Program.cs
@@ -8,8 +8,28 @@ namespace DbFirst
 {
     internal class Program
     {
+        public enum Level: byte
+        {
+            Nuevo = 1,
+            Intermedio = 2,
+            Adelantado= 3
+        }
         static void Main(string[] args)
         {
+
+            var dbContext = new PlutoDbContext();
+
+            dbContext.GetAuthorCourses(1);
+
+            var courses = dbContext.GetCourses();
+
+            foreach (var item in courses)
+            {
+                Console.WriteLine(  item.Title);
+            }
+
+            var course = new Courses();
+            course.Level = CourseLevel.Begginer;
         }
     }
 }
